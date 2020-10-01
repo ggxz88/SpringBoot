f5bc67b08b086f14b2bd43d8dd7ebf954f238739
PARK HEESEON <ggxz88@naver.com>
2020-10-01 15:03:42 +0900

    데이터 전달자 모델

--- DevProject/src/main/java/org/hdcd/controller/MemberController.java
+++ DevProject/src/main/java/org/hdcd/controller/MemberController.java
@@ -1,6 +1,7 @@
 package org.hdcd.controller;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
@@ -15,6 +16,8 @@ import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,6 +25,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @RestController
 @Controller
@@ -995,7 +999,7 @@ public class MemberController {
 	*/
 	
 	//파일업로드 Ajax 방식 요청 처리
-	
+	/*
 	@RequestMapping(value = "/uploadAjax", method = RequestMethod.POST, produces = "text/plain; charset=UTF-8")
 	public ResponseEntity<String> uploadAjax(MultipartFile file) throws Exception {
 		String originalFilename = file.getOriginalFilename();
@@ -1004,5 +1008,316 @@ public class MemberController {
 		
 		ResponseEntity<String> entity = new ResponseEntity<String>("UPLOAD SUCCESS" + originalFilename, HttpStatus.OK);
 		
-		return entity;	}
+		return entity;	
+	}
+	*/
+	
+	//6. 데이터 전달자 모델
+	//모델을 통한 데이터 전달
+	/*
+	@RequestMapping(value = "/read01", method = RequestMethod.GET)
+	public String read01(Model model) {
+		logger.info("read01");
+		
+		model.addAttribute("userId", "hongkd");
+		model.addAttribute("password", "1234");
+		model.addAttribute("email", "aaa@ccc.com");
+		model.addAttribute("userName", "홍길동");
+		model.addAttribute("birthDay", "1989-09-07");
+		
+		return "read01";
+	}
+	
+	@RequestMapping(value = "/read02", method = RequestMethod.GET)
+	public String read02(Model model) {
+		logger.info("read02");
+		
+		Member member = new Member();
+		
+		member.setUserId("hongkd");
+		member.setPassword("1234");
+		member.setEmail("aaa@ccc.com");
+		member.setUserName("홍길동");
+		member.setBirthDay("1989-09-07");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, 1988);
+		cal.set(Calendar.MONTH, 10);
+		cal.set(Calendar.DAY_OF_MONTH, 7);
+		
+		member.setDateOfBirth(cal.getTime());
+		
+		model.addAttribute(member);
+
+		return "read02";
+	}
+	
+	@RequestMapping(value = "/read03", method = RequestMethod.GET)
+	public String read03(Model model) {
+		logger.info("read03");
+		
+		Member member = new Member();
+		
+		member.setUserId("hongkd");
+		member.setPassword("1234");
+		member.setEmail("aaa@ccc.com");
+		member.setUserName("홍길동");
+		member.setBirthDay("1989-09-07");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, 1988);
+		cal.set(Calendar.MONTH, 10);
+		cal.set(Calendar.DAY_OF_MONTH, 7);
+		
+		member.setDateOfBirth(cal.getTime());
+		
+		model.addAttribute("user", member);
+
+		return "read03";
+	}
+	
+	@RequestMapping(value = "/read04", method = RequestMethod.GET)
+	public String read04(Model model) {
+		logger.info("read04");
+		
+		String[] carArray = {"saab", "audi"};
+		
+		List<String> carList = new ArrayList<String>();
+		carList.add("saab");
+		carList.add("audi");
+		
+		String[] hobbyArray = {"Music", "Movie"};
+		
+		List<String> hobbyList = new ArrayList<String>();
+		hobbyList.add("Music");
+		hobbyList.add("Movie");
+		
+		model.addAttribute("carArray", carArray);
+		model.addAttribute("carList", carList);
+
+		model.addAttribute("hobbyArray", hobbyArray);
+		model.addAttribute("hobbyList", hobbyList);
+
+		return "read04";
+	}
+	
+	@RequestMapping(value = "/read05", method = RequestMethod.GET)
+	public String read05(Model model) {
+		logger.info("read05");
+		
+		Member member = new Member();
+		
+		Address address = new Address();
+		address.setPostCode("080908");
+		address.setLocation("seoul");
+		
+		member.setAddress(address);
+		
+		List<Card> cardList = new ArrayList<Card>();
+		
+		Card card1 = new Card();
+		card1.setNo("123456");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, 2020);
+		cal.set(Calendar.MONTH, 9);
+		cal.set(Calendar.DAY_OF_MONTH, 8);
+		
+		card1.setValidMonth(cal.getTime());
+		
+		cardList.add(card1);
+		
+		Card card2 = new Card();
+		card2.setNo("456789");
+		
+		cal.set(Calendar.YEAR, 2022);
+		cal.set(Calendar.MONTH, 11);
+		cal.set(Calendar.DAY_OF_MONTH, 7);
+		
+		card2.setValidMonth(cal.getTime());
+		
+		cardList.add(card2);
+		
+		member.setCardList(cardList);
+		
+		model.addAttribute("user", member);
+		
+		return "read05";
+	}
+	
+	@RequestMapping(value = "/read06", method = RequestMethod.GET)
+	public String read06(Model model) {
+		logger.info("read06");
+		
+		Member member = new Member();
+		
+		member.setUserId("hongkd");
+		member.setPassword("1234");
+		member.setEmail("aaa@ccc.com");
+		member.setUserName("홍길동");
+		member.setBirthDay("1989-09-07");
+		
+		member.setGender("female");
+		
+		member.setDeveloper("Y");
+		member.setForeigner(true);
+		
+		member.setNationality("Austraila");
+		
+		//user.setCars("saab, audi");
+		member.setCars("saab");
+		
+		String[] carArray = {"saab", "audi"};
+		
+		member.setCarArray(carArray);
+		
+		List<String> carList = new ArrayList<String>();
+		carList.add("saab");
+		carList.add("audi");
+		
+		member.setCarList(carList);
+		
+		member.setHobby("Movie");
+		
+		String[] hobbyArray = {"Music", "Movie"};
+		
+		member.setHobbyArray(hobbyArray);
+				
+		List<String> hobbyList = new ArrayList<String>();
+		hobbyList.add("Music");
+		hobbyList.add("Movie");
+		
+		member.setHobbyList(hobbyList);
+
+		Address address = new Address();
+		address.setPostCode("080908");
+		address.setLocation("seoul");
+		
+		member.setAddress(address);
+		
+		List<Card> cardList = new ArrayList<Card>();
+		
+		Card card1 = new Card();
+		card1.setNo("123456");
+		
+		Calendar cal = Calendar.getInstance();
+		cal.set(Calendar.YEAR, 2020);
+		cal.set(Calendar.MONTH, 9);
+		cal.set(Calendar.DAY_OF_MONTH, 8);
+		
+		card1.setValidMonth(cal.getTime());
+		
+		cardList.add(card1);
+		
+		Card card2 = new Card();
+		card2.setNo("456789");
+		
+		cal.set(Calendar.YEAR, 2022);
+		cal.set(Calendar.MONTH, 11);
+		cal.set(Calendar.DAY_OF_MONTH, 7);
+		
+		card2.setValidMonth(cal.getTime());
+		
+		cardList.add(card2);
+		
+		member.setCardList(cardList);
+		
+		cal.set(Calendar.YEAR, 1988);
+		cal.set(Calendar.MONTH, 10);
+		cal.set(Calendar.DAY_OF_MONTH, 7);
+		
+		member.setDateOfBirth(cal.getTime());
+		
+		String introduction = "안녕하세요.\n반갑습니다.";
+		
+		member.setIntroduction(introduction);
+		
+		model.addAttribute("user", member);
+
+		return "read06";
+	}
+	*/
+	
+	//@ModelAttribute 애너테이션
+	/*
+	@RequestMapping(value = "/registerForm", method = RequestMethod.GET)
+	public String registerForm() {
+		logger.info("registerForm");
+		
+		return "registerForm";
+	}
+	
+	@RequestMapping(value = "/register01", method = RequestMethod.POST)
+	public String register01(String userId) {
+		logger.info("register01");
+		
+		logger.info("userId = " + userId);
+		// > userId = hongkd
+		
+		return "result01";
+	}
+	
+	@RequestMapping(value = "/register02", method = RequestMethod.POST)
+	public String register02(@ModelAttribute("userId") String userId) {
+		logger.info("register02");
+		
+		logger.info("userId = " + userId);
+		// > userId = hongkd
+		
+		return "result02";
+	}
+	
+	@RequestMapping(value = "/register03", method = RequestMethod.POST)
+	public String register03(@ModelAttribute("userId") String userId, @ModelAttribute("password") String password) {
+		logger.info("register03");
+		
+		logger.info("userId = " + userId);
+		// > userId = hongkd
+		
+		logger.info("password = " + password);
+		// > password = 1234
+		
+		return "result03";
+	}
+	
+	@RequestMapping(value = "/register04", method = RequestMethod.POST)
+	public String register04(Member member) {
+		logger.info("register04");
+		
+		logger.info("userId = " + member.getUserId());
+		// > userId = hongkd
+		
+		logger.info("password = " + member.getPassword());
+		// > password = 1234
+		
+		return "result04";
+	}
+	*/
+	
+	//RedirectAttributes 타입
+	
+	@RequestMapping(value = "/registerForm", method = RequestMethod.GET)
+	public String registerForm() {
+		logger.info("registerForm");
+		
+		return "registerForm";
+	}
+	
+	@RequestMapping(value = "/register", method = RequestMethod.POST)
+	public String register(Member member, RedirectAttributes rttr) throws Exception {
+		logger.info("register");
+		
+		rttr.addFlashAttribute("msg", "success");
+		
+		return "registerForm";
+	}
+	
+	@RequestMapping(value = "/result", method = RequestMethod.GET)
+	public String result() {
+		logger.info("result");
+		
+		return "result";
+	}
+	
+	
 }
