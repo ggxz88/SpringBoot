028622fd908e5fe3dfe61be8f71fbc373dcd19a6
PARK HEESEON <ggxz88@naver.com>
2020-10-24 00:13:54 +0900

    스프링부트 실전 - 관리자/마이페이지

---
+++ Project/src/main/java/org/hdcd/controller/MypageController.java
@@ -0,0 +1,40 @@
+package org.hdcd.controller;
+
+import org.hdcd.common.security.domain.CustomUser;
+import org.hdcd.domain.Member;
+import org.hdcd.service.MemberService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/mypage")
+public class MypageController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(MypageController.class);
+	
+	@RequestMapping(value = "/admin", method = RequestMethod.GET)
+	public String admin() {
+		logger.info("Adminpage");
+		
+		return "mypage/admin";
+	}
+
+	@RequestMapping(value = "/my", method = RequestMethod.GET)
+	public String my(Model model, Authentication authentication) throws Exception {
+		logger.info("Mypage");
+		
+		CustomUser customUser = (CustomUser) authentication.getPrincipal();
+		Member member = customUser.getMember();
+		
+		String userId = member.getUserId();
+		
+		return "mypage/my";
+	}
+
+}
